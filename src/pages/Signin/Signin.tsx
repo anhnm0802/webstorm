24a631cea96930aae2454a2976c473451adbbed5
anhnm0802 <anhnm0802@gmail.com>
2024-11-11 16:42:22 +0700

    update signin + download axios

--- src/pages/Signin/Signin.tsx
+++ src/pages/Signin/Signin.tsx
@@ -1,22 +1,182 @@
-import { Box, Grid, Paper } from "@mui/material"
-import { Trans } from "react-i18next"
-
+import {
+  Box,
+  Button,
+  Grid,
+  IconButton,
+  InputAdornment,
+  Link,
+  Paper,
+  TextField,
+  Typography,
+} from "@mui/material";
+import { useState } from "react";
+import { Trans } from "react-i18next";
+import VisibilityOutlinedIcon from "@mui/icons-material/VisibilityOutlined";
+import VisibilityOffOutlinedIcon from "@mui/icons-material/VisibilityOffOutlined";
 const Signin = () => {
-    return (
-        <>
-           <Grid sx={{height:'100vh'}}>
-                <Grid xs={12} sm={8} md={6} component={Paper} elevation={3} >
-                    <Box sx={{
-                        my:8,
-                        mx:4,
-                        display:"flex"
+  const [visiblePw, setVisiblePw] = useState(false);
+  const handleClickShowPassword = () => {
+    setVisiblePw(!visiblePw);
+  };
+  const [valueinput, setValueinput] = useState("");
 
-                    }}>
-                        test
-                    </Box>
-                </Grid>
-           </Grid>
-        </>
-    )
-}
-export default Signin
+  return (
+    <>
+      <Grid container sx={{ height: "100vh" }} justifyContent={"center"}>
+        <Grid
+          item
+          container
+          xs={12}
+          sm={10}
+          md={8}
+          lg={8}
+          component={Paper}
+          elevation={6}
+          square
+        >
+          <Grid
+            item
+            xs={12}
+            sm={6}
+            md={6}
+            lg={6}
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            Logo
+          </Grid>
+          <Grid
+            item
+            xs={12}
+            sm={6}
+            md={6}
+            lg={6}
+            sx={{
+              display: "flex",
+            }}
+          >
+            <Box
+              sx={{
+                my: 3,
+                mx: 3,
+                width: "100%",
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            >
+              <Typography variant="h6">
+                <Trans>Login</Trans>
+              </Typography>
+              <Box width={"100%"} sx={{ mt: 2, mb: 2 }}>
+                <Typography sx={{ mb: 1 }}>Email or phone number</Typography>
+                <TextField
+                  sx={{ width: "100%", my: 1 }}
+                  label="username"
+                  value={valueinput}
+                  onChange={(event) => setValueinput(event.target.value)}
+                />
+                <Typography sx={{ mb: 1 }}>Password</Typography>
+                <TextField
+                  sx={{ width: "100%", my: 1 }}
+                  InputProps={{
+                    endAdornment: (
+                      <InputAdornment position="end">
+                        <IconButton onClick={handleClickShowPassword}>
+                          {visiblePw ? (
+                            <VisibilityOffOutlinedIcon />
+                          ) : (
+                            <VisibilityOutlinedIcon />
+                          )}
+                        </IconButton>
+                      </InputAdornment>
+                    ),
+                  }}
+                  type={visiblePw ? "text" : "password"}
+                  label="Password"
+                  onChange={(event) => setValueinput(event.target.value)}
+                ></TextField>
+                <Link
+                  href="/"
+                  sx={{ color: "black", fontWeight: "700" }}
+                  underline="none"
+                >
+                  Forgot Password?
+                </Link>
+              </Box>
+              <Button
+                sx={{
+                  bgcolor: "gray",
+                  color: "white",
+                  width: "100%",
+                  "&:hover": {
+                    color: "black",
+                    background: "#cccccc",
+                  },
+                }}
+              >
+                Login
+              </Button>
+              <Box display={"flex"} width={"100%"} alignItems={"center"}>
+                <Box
+                  flex={1}
+                  sx={{ height: "0px", border: "1px solid black" }}
+                ></Box>
+                <Typography sx={{ mx: 2, my: 2 }}>Login with</Typography>
+                <Box
+                  flex={1}
+                  sx={{ height: "0px", border: "1px solid black" }}
+                ></Box>
+              </Box>
+              <Button
+                sx={{
+                  bgcolor: "gray",
+                  color: "white",
+                  width: "100%",
+                  "&:hover": {
+                    color: "black",
+                    background: "#cccccc",
+                  },
+                }}
+              >
+                Google
+              </Button>
+              <Button
+                sx={{
+                  bgcolor: "gray",
+                  color: "white",
+                  width: "100%",
+                  "&:hover": {
+                    color: "black",
+                    background: "#cccccc",
+                  },
+                }}
+              >
+                Facebook
+              </Button>
+              <Button
+                sx={{
+                  bgcolor: "gray",
+                  color: "white",
+                  width: "100%",
+                  "&:hover": {
+                    color: "black",
+                    background: "#cccccc",
+                  },
+                }}
+                startIcon={<></>}
+              >
+                Github
+              </Button>
+            </Box>
+          </Grid>
+        </Grid>
+      </Grid>
+    </>
+  );
+};
+export default Signin;
